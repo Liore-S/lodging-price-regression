58303d3ecc907c2dc097fe872c2c160e6491e406
Liore-S <goober395@gmail.com>
2023-01-21 20:23:00 +0700

    edit: remove unused package

--- App/app.py
+++ App/app.py
@@ -1,9 +1,6 @@
 # import variable from data-processing.py
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pickle
 
 # Load Variable
